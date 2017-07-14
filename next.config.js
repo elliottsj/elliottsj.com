0a205b221811266c6a7934d16f35454b727571fe
Spencer Elliott <me@elliottsj.com>
2017-07-14 14:10:21 -0400

    Use babel-inline-import-loader and npm@5

--- next.config.js
+++ next.config.js
@@ -1,10 +1,34 @@
+'use strict';
+
 module.exports = {
   webpack: config => {
-    return Object.assign({}, config, {
+    const rulesExceptBabelLoaderRule = config.module.rules.slice(0, -1);
+    const babelLoaderRule = config.module.rules.slice(-1)[0];
+    const newConfig = Object.assign({}, config, {
       node: {
         __dirname: true,
         __filename: true,
       },
+      module: Object.assign({}, config.module, {
+        rules: [
+          ...rulesExceptBabelLoaderRule,
+          {
+            test: babelLoaderRule.test,
+            include: babelLoaderRule.include,
+            exclude: babelLoaderRule.exclude,
+            use: [
+              'babel-inline-import-loader',
+              {
+                loader: 'babel-loader',
+                options: Object.assign({}, babelLoaderRule.options, {
+                  cacheDirectory: false,
+                }),
+              },
+            ],
+          },
+        ],
+      }),
     });
+    return newConfig;
   },
 };
